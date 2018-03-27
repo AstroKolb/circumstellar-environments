cb7f6666372179af69417aec5464fdb1471a3167
AstroKolb <astro.cekolb@gmail.com>
2018-03-27 00:20:19 -0400

    variable update

--- src/PPMLR/vh1mods.f90
+++ src/PPMLR/vh1mods.f90
@@ -15,13 +15,29 @@ module global
  real :: time, dt, timem, timep, svel 
  real :: gam, pi, gamm
  real, parameter :: courant = 0.5        ! timestep fraction of courant limit
- real, parameter :: xwig = 0.05          ! fraction of a zone to wiggle grid for dissipation
- real, parameter :: smallp = 1.0e-05     ! Set small values to prevent divide by zero
- real, parameter :: smallr = 1.0e-05
- real, parameter :: small  = 1.0e-15
+ real, parameter :: xwig = 0.00          ! fraction of a zone to wiggle grid for dissipation
+ real, parameter :: smallp = 1.0e-45     ! Set small values to prevent divide by zero
+ real, parameter :: smallr = 1.0e-45
+ real, parameter :: small  = 1.0e-45
+
+
+ ! constants
+ real, parameter :: GRV  = 6.67e-08
+ real, parameter :: Msun = 1.99e+33
+ real, parameter :: Rsun = 6.95e+10
+ real, parameter :: Lsun = 3.89e+33
+ real, parameter :: AU   = 1.50e+13
+ real, parameter :: mp   = 1.67e-24
+ real, parameter :: kB   = 1.67e-16
+ real, parameter :: GM   = GRV*Msun
 
  real :: uinflo, dinflo, vinflo, winflo, pinflo, einflo 
  real :: uotflo, dotflo, votflo, wotflo, potflo, eotflo
+
+ real :: GMP
+ real :: Tmp, cs2, uc, rc, rho, capI
+
+ real, dimension(6) :: uin
       
 end module 
 
