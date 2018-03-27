d4cc6e0f42dbc146b103c9efaeedf1c7be99920b
AstroKolb <astro.cekolb@gmail.com>
2018-03-27 13:09:44 -0400

    adding secondary gravity, rotational pseudoforces

--- src/PPMLR/forces.f90
+++ src/PPMLR/forces.f90
@@ -12,36 +12,112 @@ IMPLICIT NONE
 
 ! LOCALS
 integer :: n
-real :: xf0, sinxf0
+real :: xf0, sinxf0, radS
 real, dimension(maxsweep) :: grav, fict, xf
 
 !--------------------------------------------------------------
-
-xf0 = xf0
  
 if (sweep == 'x') then
 
-   do n = nmin-4, nmax+5
-      grav(n) = - GMP / xf(n)**2
+   if (yin) then
+      do n = nmin-4, nmax+5
+         radS = sqrt(xf(n)**2 + sep**2 - 2.0*xf(n)*sep*stheta*cphi)
 
-      fict(n) = (w(n)*w(n)+v(n)*v(n))/xf(n)
-   enddo
+         grav(n) = - GMP / xf(n)**2                &
+                   - GMS / radS**3 * (xf(n)-sep*stheta*cphi)
+
+         fict(n) = + (w(n)*w(n)+v(n)*v(n))/xf(n)   &
+                   + omega**2*xf(n)*stheta**2      &
+                   - omega**2*rcm*stheta*cphi      &
+                   + omega*2.0*stheta*w(n)         &
+                   + 0.0
+      enddo
+   else
+      do n = nmin-4, nmax+5
+         radS = sqrt(xf(n)**2 + sep**2 + 2.0*xf(n)*sep*stheta*cphi)
+
+         grav(n) = - GMP / xf(n)**2                               &
+                   - GMS / radS**3 * (xf(n)+sep*stheta*cphi)
+
+         fict(n) = + (w(n)*w(n)+v(n)*v(n))/xf(n)                  &
+                   + omega**2*xf(n)*(ctheta**2*sphi**2+cphi**2)   &
+                   + omega**2*rcm*stheta*cphi                     &
+                   - omega*2.0*(sphi*ctheta*w(n)-cphi*v(n))       &
+                   + 0.0
+      enddo
+   endif     
 
 else if (sweep == 'y') then
 
-   do n = nmin-4, nmax+5
-      grav(n) = 0.0d0
+   if (yin) then
+      do n = nmin-4, nmax+5
+         stheta = sin(xf(n))
+         ctheta = cos(xf(n))
+         radS = sqrt(radius**2 + sep**2 - 2.0*radius*sep*stheta*cphi)
 
-      fict(n) = v(n)*v(n)*cos(xf(n))/(radius*sin(xf(n))) - u(n)*w(n)/radius
-   enddo
+         grav(n) = + 0.0d0                                     &
+                   + GMS / radS**3 * sep*ctheta*cphi
+
+         fict(n) = + v(n)*v(n)*cos(xf(n))/(radius*sin(xf(n)))  &
+                   - u(n)*w(n)/radius                          &
+                   + omega**2*radius*stheta*ctheta             &
+                   - omega**2*rcm*ctheta*cphi                  &
+                   + omega*2.0*ctheta*v(n)                     &
+                   + 0.0
+      enddo
+   else
+      do n = nmin-4, nmax+5
+         stheta = sin(xf(n))
+         ctheta = cos(xf(n))
+         radS = sqrt(radius**2 + sep**2 + 2.0*radius*sep*stheta*cphi)
+
+         grav(n) = + 0.0d0                                     &
+                   - GMS / radS**3 * sep*ctheta*cphi
+
+         fict(n) = + v(n)*v(n)*cos(xf(n))/(radius*sin(xf(n)))  &
+                   - u(n)*w(n)/radius                          &
+                   - omega**2*radius*sphi**2*ctheta*stheta     &
+                   + omega**2*rcm*ctheta*cphi                  &
+                   - omega*2.0*(cphi*w(n)-sphi*stheta*v(n))    &
+                   + 0.0
+      enddo
+   endif
 
 else
 
-   do n = nmin-4, nmax+5
-      grav(n) = 0.0d0
-      
-      fict(n) = - u(n)*w(n)/radius*cos(theta) - u(n)*v(n)/radius*stheta 
-   enddo
+   if (yin) then
+      do n = nmin-4, nmax+5
+         sphi = sin(xf(n))
+         cphi = cos(xf(n))
+         radS = sqrt(radius**2/stheta**2 + sep**2 - 2.0*radius*sep*cphi)
+
+         grav(n) = + 0.0d0                                  &
+                   - GMS / radS**3 * sep*sphi
+         
+         fict(n) = - u(n)*w(n)/radius*cos(theta)            &
+                   - u(n)*v(n)/radius*stheta                &
+                   + 0.0                                    &
+                   + omega**2*rcm*sphi                      &
+                   - omega*2.0*(stheta*v(n)+ctheta*w(n))    &
+                   + 0.0
+      enddo
+   else
+      do n = nmin-4, nmax+5
+         sphi = sin(xf(n))
+         cphi = cos(xf(n))
+         radS = sqrt(radius**2/stheta**2 + sep**2 + 2.0*radius*sep*cphi)
+
+         grav(n) = + 0.0d0                                           &
+                   + GMS / radS**3 * sep*sphi
+         
+         fict(n) = - u(n)*w(n)/radius*cos(theta)                     &
+                   - u(n)*v(n)/radius*stheta                         &
+                   - omega**2*radius*cphi*sphi                       &
+                   - omega**2*rcm*sphi                               &
+                   - omega*2.0*(stheta*sphi*w(n)-ctheta*sphi*v(n))   &
+                   + 0.0
+      enddo
+   endif
 
 endif
 
